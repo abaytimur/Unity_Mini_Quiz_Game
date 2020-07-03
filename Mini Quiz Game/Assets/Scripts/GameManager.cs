aabc35314797fd8ad473b05458ccf4046165d27e
Kadir <a_baytimur@hotmail.com>
2020-07-03 18:35:38 +0300

    Add project files

---
+++ Mini Quiz Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Question[] questions;
+
+    private static List<Question> _unAnsweredQuestions;
+
+    private Question activeQuestion;
+
+    [SerializeField] private Text questionNumberText;
+    [SerializeField] private GameObject buttonDisablePanel;
+    [SerializeField] private GameObject resultPanel;
+    [SerializeField] private Text questionText;
+    [SerializeField] private Text correctAnswerText, wrongAnswerText;
+    [SerializeField] private GameObject correctButton, wrongButton;
+    private RectTransform rectTransform;
+    private RectTransform rectTransform1;
+
+    private int correctAnswersNumber, wrongAnswersNumber;
+    private int totalPoints;
+    private int totalQuestionNumber;
+    
+    private ResultManager resultManager;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rectTransform1 = correctButton.GetComponent<RectTransform>();
+        rectTransform = wrongButton.GetComponent<RectTransform>();
+    }
+
+    void Start()
+    {
+        if (_unAnsweredQuestions == null || _unAnsweredQuestions.Count == 0)
+        {
+            _unAnsweredQuestions = questions.ToList<Question>();
+        }
+
+        totalQuestionNumber = _unAnsweredQuestions.Count;
+        questionNumberText.text = totalQuestionNumber - _unAnsweredQuestions.Count + " / " + totalQuestionNumber;
+        correctAnswersNumber = 0;
+        wrongAnswersNumber = 0;
+        totalPoints = 0;
+
+        PickRandomQuestion();
+    }
+
+    private void PickRandomQuestion()
+    {
+        rectTransform.DOLocalMoveX(320f, .2f);
+        rectTransform1.DOLocalMoveX(-320f, .2f);
+
+        int randomQuestionIndex = Random.Range(0, _unAnsweredQuestions.Count);
+        activeQuestion = _unAnsweredQuestions[randomQuestionIndex];
+
+        questionText.text = activeQuestion.question;
+
+        if (activeQuestion.isItCorrect)
+        {
+            correctAnswerText.text = "Correct Answer!";
+            wrongAnswerText.text = "Wrong Answer!";
+        }
+        else
+        {
+            correctAnswerText.text = "Wrong Answer!";
+            wrongAnswerText.text = "Correct Answer!";
+        }
+    }
+
+    IEnumerator WaitBetweenQuestions()
+    {
+        _unAnsweredQuestions.Remove(activeQuestion);
+        yield return new WaitForSeconds(1f);
+
+        if (_unAnsweredQuestions.Count <= 0)
+        {
+            resultPanel.SetActive(true);
+            resultManager = Object.FindObjectOfType<ResultManager>();
+            resultManager.ShowResults(correctAnswersNumber, wrongAnswersNumber, totalPoints);
+        }
+        else
+        {
+            PickRandomQuestion();
+        }
+    }
+
+    public void CorrectButtonIsClicked()
+    {
+        if (activeQuestion.isItCorrect)
+        {
+            correctAnswersNumber++;
+            totalPoints += 100;
+        }
+        else
+        {
+            wrongAnswersNumber++;
+            totalPoints -= 25;
+            if (totalPoints <= 0)
+            {
+                totalPoints = 0;
+            }
+        }
+        StartCoroutine(ButtonDisablePanel());
+        wrongButton.GetComponent<RectTransform>().DOLocalMoveX(1000f, .2f);
+        StartCoroutine(WaitBetweenQuestions());
+        questionNumberText.text = totalQuestionNumber - _unAnsweredQuestions.Count + " / " + totalQuestionNumber;
+       
+    }
+
+    public void WrongButtonIsClicked()
+    {
+        if (!activeQuestion.isItCorrect)
+        {
+            correctAnswersNumber++;
+            totalPoints += 100;
+        }
+        else
+        {
+            wrongAnswersNumber++;
+            totalPoints -= 25;
+        }
+        StartCoroutine(ButtonDisablePanel());
+        StartCoroutine(WaitBetweenQuestions());
+        correctButton.GetComponent<RectTransform>().DOLocalMoveX(-1000f, .2f);
+        questionNumberText.text = totalQuestionNumber - _unAnsweredQuestions.Count + " / " + totalQuestionNumber;
+        
+    }
+
+    IEnumerator ButtonDisablePanel()
+    {
+        buttonDisablePanel.SetActive(true);
+        yield return new WaitForSeconds(1.25f);
+        buttonDisablePanel.SetActive(false);
+    }
+}
---
+++ Mini Quiz Game/Assets/Scripts/ResultManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    [SerializeField] private Text correctAnswersNumberText, wrongAnswersNumberText, resultPointsText;
+    [SerializeField] private GameObject leftStar, middleStar, rightStar;
+    [SerializeField] private GameObject resultPanel;
+    private string url = "https://forms.gle/hMBkUYED6bmxkmPu6";
+
+    public void ShowResults(int correctNumber, int wrongNumber, int points)
+    {
+        correctAnswersNumberText.text = correctNumber.ToString();
+        wrongAnswersNumberText.text = wrongNumber.ToString();
+        resultPointsText.text = points.ToString();
+
+        leftStar.SetActive(false);
+        middleStar.SetActive(false);
+        rightStar.SetActive(false);
+
+        if (correctNumber > 6 && correctNumber <= 13) 
+        {
+            middleStar.SetActive(true);
+        }
+        else if (correctNumber <= 19)
+        {
+            leftStar.SetActive(true);
+            middleStar.SetActive(true);
+        }
+        else
+        {
+            leftStar.SetActive(true);
+            middleStar.SetActive(true);
+            rightStar.SetActive(true);
+        }
+    }
+
+    public void RestartGame()
+    {
+        // SceneManager.LoadScene(sceneBuildIndex: 0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        resultPanel.SetActive(false);
+    }
+
+    public void FeedbackButton()
+    {
+        Application.OpenURL(url);
+    }
+}
